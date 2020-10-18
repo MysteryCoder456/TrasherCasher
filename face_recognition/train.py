fead7e5c9d1968cff44ebc34d1d3015828f0a0cd
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-10-18 22:25:09 +0400

    added face recognition

---
+++ face_recognition/train.py
@@ -0,0 +1,47 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from skimage import io, transform
+import matplotlib.pyplot as plt
+
+root_folder = "face_recognition"
+
+training_data = []
+training_labels = []
+
+class_names = ["Rehatbir Singh", "Keanu Reeves", "Navneet Kaur"]
+
+class_index = 0
+
+for class_name in os.listdir(f"{root_folder}/training_data"):
+    if class_name != ".DS_Store":
+        for filename in os.listdir(f"{root_folder}/training_data/{class_name}"):
+            if filename != ".DS_Store":
+                image = io.imread(f"{root_folder}/training_data/{class_name}/{filename}", as_gray=True)
+                resized = transform.resize(image, (200, 300))
+                training_data.append(resized)
+                training_labels.append(class_index)
+        class_index += 1
+
+print(class_names)
+
+# shows all training images
+# for i in range(len(training_data)):
+#     plt.xlabel(training_labels[i])
+#     plt.imshow(training_data[i])
+#     plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(200, 300)),
+    keras.layers.Dense(256, activation="relu"),
+    keras.layers.Dense(256, activation="relu"),
+    keras.layers.Dense(512, activation="relu"),
+    keras.layers.Dense(256, activation="relu"),
+    keras.layers.Dense(len(class_names), activation="softmax")
+])
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+# model.fit(training_data, training_labels, epochs=5)
+model.fit(np.array(training_data), np.array(training_labels), epochs=50)
+model.save(f"{root_folder}/face_recog_model.h5")
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from tensorflow import keras
+import numpy as np
 import cv2
+from skimage import transform
+
+class_names = ["Rehatbir Singh", "Keanu Reeves", "Navneet Kaur"]
+face_recog = keras.models.load_model("face_recognition/face_recog_model.h5")
 
 cap = cv2.VideoCapture(0)
 face_classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
@@ -6,11 +12,15 @@ face_classifier_alt = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 
 while True:
     _, frame = cap.read()
-    faces = face_classifier.detectMultiScale(
-        frame, minSize=(120, 120), minNeighbors=4)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(frame, minSize=(120, 120), minNeighbors=4)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), 3, 3)
+        cropped = frame[x:x + w, y:y + h]
+        cropped = transform.resize(frame, (200, 300))
+        prediction = face_recog.predict(np.expand_dims(cropped, 0))
+        print(class_names[np.argmax(prediction)])
+        cv2.rectangle(frame, (x, y), (x + w, y + h), 3, 3)
 
     cv2.imshow("Camera Footage", frame)
 
