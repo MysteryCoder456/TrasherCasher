e135fd9473fe525fcf4582c36f9cfae948229a8f
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-10-20 22:15:04 +0400

    new data

--- trash_detector/test.py
+++ trash_detector/test.py
@@ -11,12 +11,17 @@ model = keras.models.load_model("trash_detector/trash_detector_model.h5")
 # load testing data
 for filename in os.listdir(f"trash_detector/testing_data"):
     if filename != ".DS_Store":
-        image = io.imread(f"trash_detector/testing_data/{filename}", as_gray=True)
+        image = io.imread(f"trash_detector/testing_data/{filename}", as_gray=True) / 255
         resized = transform.resize(image, RESIZE_RES)
         predictions = model.predict(np.expand_dims(resized, 0))
 
         # display image with prediction
+        plt.subplot(2, 1, 1)
         plt.title(f"Prediciction Label: {class_names[np.argmax(predictions)]}")
         plt.xlabel(f"Confidence: {max(predictions)}")
         plt.imshow(image, cmap=plt.cm.binary)
+        plt.subplot(2, 1, 2)
+        plt.title(f"Prediciction Label: {class_names[np.argmax(predictions)]}")
+        plt.xlabel(f"Confidence: {max(predictions)}")
+        plt.imshow(resized, cmap=plt.cm.binary)
         plt.show()
--- trash_detector/train.py
+++ trash_detector/train.py
@@ -19,7 +19,7 @@ for class_name in os.listdir("trash_detector/training_data"):
     if class_name != ".DS_Store":
         for filename in os.listdir(f"trash_detector/training_data/{class_name}"):
             if class_name != ".DS_Store":
-                image = io.imread(f"trash_detector/training_data/{class_name}/{filename}", as_gray=True)
+                image = io.imread(f"trash_detector/training_data/{class_name}/{filename}", as_gray=True) / 255
                 resized = transform.resize(image, RESIZE_RES)
                 training_data.append(resized)
                 training_labels.append(class_names.index(class_name))
@@ -27,14 +27,17 @@ for class_name in os.listdir("trash_detector/training_data"):
 # create model
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=RESIZE_RES),
+    keras.layers.Dense(265, activation="relu"),
     keras.layers.Dense(256, activation="relu"),
-    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(256, activation="relu"),
+    keras.layers.Dense(256, activation="relu"),
+    keras.layers.Dense(256, activation="relu"),
     keras.layers.Dense(256, activation="relu"),
     keras.layers.Dense(2, activation="softmax")
 ])
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-# train model
+# train and save model
 model.fit(np.array(training_data), np.array(training_labels), epochs=15, batch_size=4)
 model.save("trash_detector/trash_detector_model.h5")
