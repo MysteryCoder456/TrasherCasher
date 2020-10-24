60f114a1f46a4d6080c03e2992a3361c3bdaa727
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-10-24 12:26:19 +0400

    few prints statements for debugging

--- trash_detector/test.py
+++ trash_detector/test.py
@@ -8,9 +8,10 @@ class_names = ["clean", "trash"]
 RESIZE_RES = (224, 224)
 model = keras.models.load_model("trash_detector/trash_detector_model.h5")
 folder_size = len(os.listdir("trash_detector/testing_data"))
-data = np.ndarray(shape=(folder_size - 1, 224, 224, 3), dtype=np.float32)
+data = np.ndarray(shape=(folder_size, 224, 224, 3), dtype=np.float32)
 
 # load testing data
+print("loading testing images...")
 index = 0
 for filename in os.listdir("trash_detector/testing_data"):
     if filename != ".DS_Store":
@@ -20,13 +21,13 @@ for filename in os.listdir("trash_detector/testing_data"):
         normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
         data[index] = normalized_image_array
         index += 1
-
+print("testing images loaded!")
 
 predictions = model.predict(data)
 
 for i in range(len(predictions)):
     # display image with prediction
-    plt.title(f"Prediciction Label: {class_names[np.argmax(predictions[i])]}")
+    plt.title(f"Prediciction: {class_names[np.argmax(predictions[i])]}")
     plt.xlabel(f"Confidence: {max(predictions[i])}")
     plt.imshow(data[i])
     plt.show()
