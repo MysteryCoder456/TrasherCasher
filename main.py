0cf84c9d2cb02bbab4a1ffaf3aff7313b69dabca
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-10-25 14:06:23 +0400

    added command-line mode

--- main.py
+++ main.py
@@ -49,50 +49,55 @@ def main():
     print("Starting camera...")
     cap = cv2.VideoCapture(0)
 
-    while True:
-        _, cam_image = cap.read()
+    try:
+        while True:
+            _, cam_image = cap.read()
 
-        # if INVERT_PIC:
-        #     cam_image = transform.rotate(cam_image, 180)
+            # if INVERT_PIC:
+            #     cam_image = transform.rotate(cam_image, 180)
 
-        cv2.imwrite("temp.jpg", cam_image)
+            cv2.imwrite("temp.jpg", cam_image)
 
-        pred_image = Image.open("temp.jpg")
-        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-        resized = ImageOps.fit(pred_image, RESIZE_RES, Image.ANTIALIAS)
-        image_array = np.asarray(resized)
-        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-        data[0] = normalized_image_array
+            pred_image = Image.open("temp.jpg")
+            data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+            resized = ImageOps.fit(pred_image, RESIZE_RES, Image.ANTIALIAS)
+            image_array = np.asarray(resized)
+            normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+            data[0] = normalized_image_array
 
-        trash_detected = trash_detector.predict(data)
-        trash_label = class_names[np.argmax(trash_detected)]
-        if DISPLAY_IMAGE:
-            cv2.putText(cam_image, trash_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, FONT_THICKNESS)
-        print(trash_label)
+            trash_detected = trash_detector.predict(data)
+            trash_label = class_names[np.argmax(trash_detected)]
+            if DISPLAY_IMAGE:
+                cv2.putText(cam_image, trash_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, FONT_THICKNESS)
+            print(trash_label)
 
-        if trash_label == "trash":
-            face_locations = face_recognition.face_locations(cam_image, model=MODEL)
-            face_encodings = face_recognition.face_encodings(cam_image, face_locations)
+            if trash_label == "trash":
+                face_locations = face_recognition.face_locations(cam_image, model=MODEL)
+                face_encodings = face_recognition.face_encodings(cam_image, face_locations)
 
-            for f_enc, f_loc in zip(face_encodings, face_locations):
-                results = face_recognition.compare_faces(known_faces, f_enc, TOLERANCE)
+                for f_enc, f_loc in zip(face_encodings, face_locations):
+                    results = face_recognition.compare_faces(known_faces, f_enc, TOLERANCE)
 
-                if True in results:
-                    match = known_names[results.index(True)]
+                    if True in results:
+                        match = known_names[results.index(True)]
 
-                    if DISPLAY_IMAGE:
-                        top_left = (f_loc[3], f_loc[0])
-                        bottom_right = (f_loc[1], f_loc[2])
-                        cv2.rectangle(cam_image, top_left, bottom_right, (0, 255, 0), FRAME_THICKNESS)
-                        cv2.putText(cam_image, match, (top_left[0], bottom_right[1] + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), FONT_THICKNESS)
+                        if DISPLAY_IMAGE:
+                            top_left = (f_loc[3], f_loc[0])
+                            bottom_right = (f_loc[1], f_loc[2])
+                            cv2.rectangle(cam_image, top_left, bottom_right, (0, 255, 0), FRAME_THICKNESS)
+                            cv2.putText(cam_image, match, (top_left[0], bottom_right[1] + 30),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), FONT_THICKNESS)
 
-                    print(f"Match found: {match}")
+                        print(f"Match found: {match}")
 
-        if DISPLAY_IMAGE:
-            cv2.imshow("Camera Footage", cam_image)
+            if DISPLAY_IMAGE:
+                cv2.imshow("Camera Footage", cam_image)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    except KeyboardInterrupt:
+        cap.release()
+        cv2.destroyAllWindows()
 
     cap.release()
     cv2.destroyAllWindows()
