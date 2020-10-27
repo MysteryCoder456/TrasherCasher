a8984f37ae26b9dd62223cc715e790121ed9e624
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-10-27 19:51:42 +0400

    message thing

--- emid.py
+++ emid.py
@@ -1,5 +1,6 @@
 class EmiratesID:
-    def __init__(self, emirates_id_number: int, name: str):
+    def __init__(self, emirates_id_number: int, name: str, phone_number: str):
         self.id = emirates_id_number
         self.name = name
+        self.phone_number = phone_number
         self.fine_amount = 0
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import os
 import json
 from tensorflow.keras.models import load_model
@@ -6,6 +7,9 @@ import cv2
 import face_recognition
 from PIL import Image, ImageOps
 from skimage import transform
+
+from RPi import GPIO
+from twilio.rest import Client
 
 from emid import EmiratesID
 
@@ -19,6 +23,7 @@ MODEL = "hog"
 INVERT_PIC = False
 RESIZE_RES = (224, 224)
 DISPLAY_IMAGE = True
+MSG_SENDER = "whatsapp:+14155238886"
 
 face_recog_folder = "face_recognition"
 known_faces = []
@@ -28,7 +33,7 @@ emirates_id_list = []
 with open("fines.json", "r") as fines_file:
     fines = json.load(fines_file)
 
-print("Loading known faces...")
+print("Loading person information...")
 
 for face_name in os.listdir(f"{face_recog_folder}/known_faces"):
     if face_name != ".DS_Store":
@@ -36,9 +41,11 @@ for face_name in os.listdir(f"{face_recog_folder}/known_faces"):
             if filename != ".DS_Store":
                 if filename == "emid.txt":
                     with open(f"{face_recog_folder}/known_faces/{face_name}/emid.txt", "r") as emid_file:
-                        id_number = int(emid_file.readlines()[0])
+                        emid_info = emid_file.readlines()
+                        id_number = int(emid_info[0])
+                        phone_number = str(emid_info[1])
+                        emirates_id = EmiratesID(id_number, face_name, phone_number)
 
-                        emirates_id = EmiratesID(id_number, face_name)
                         if str(id_number) in fines.keys():
                             emirates_id.fine_amount = fines.get(str(id_number))
 
@@ -50,10 +57,9 @@ for face_name in os.listdir(f"{face_recog_folder}/known_faces"):
                     known_faces.append(encoding)
                     known_names.append(face_name)
 
-print("Finished loading known faces!")
-
 
 def exit_sequence(video_capture):
+    GPIO.cleanup()
     video_capture.release()
     cv2.destroyAllWindows()
 
@@ -65,10 +71,50 @@ def exit_sequence(video_capture):
         json.dump(fines_dict, fines_file)
 
 
+def send_msg(name, phone_number):
+    msg_receiver = "whatsapp:" + phone_number
+    CLIENT.messages.create(
+        body=f"Dear {name}, thank you for throwing you trash it's the proper place!",
+        from_=MSG_SENDER,
+        to=msg_receiver
+    )
+
+
+def get_distance(trigger, echo):
+    GPIO.output(trigger, GPIO.HIGH)
+    time.sleep(0.0001)
+    GPIO.output(trigger, GPIO.LOW)
+
+    while not GPIO.input(echo):
+        start = time.time()
+
+    while GPIO.input(echo):
+        end = time.time()
+
+    sig_time = end - start
+    dist = sig_time / 0.000058
+    return dist
+
+
 def main():
+    print("Setting up GPIO pins...")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.cleanup()
+
+    echo_pin = 23
+    trig_pin = 24
+    led_pin = 25
+
+    GPIO.setup(echo_pin, GPIO.IN)
+    GPIO.setup(trig_pin, GPIO.OUT)
+    GPIO.setup(led_pin, GPIO.OUT)
+    GPIO.output(led_pin, GPIO.LOW)
+    previous_distance = 0
+
     print("Starting camera...")
     cap = cv2.VideoCapture(0)
     previous_trash_label = "trash"
+    match = None
 
     try:
         while True:
@@ -94,36 +140,49 @@ def main():
             else:
                 print(trash_label)
 
-            if trash_label == "trash" and previous_trash_label == "clean":
-                face_locations = face_recognition.face_locations(cam_image, model=MODEL)
-                face_encodings = face_recognition.face_encodings(cam_image, face_locations)
+            face_locations = face_recognition.face_locations(cam_image, model=MODEL)
+            face_encodings = face_recognition.face_encodings(cam_image, face_locations)
 
-                for f_enc, f_loc in zip(face_encodings, face_locations):
-                    results = face_recognition.compare_faces(known_faces, f_enc, TOLERANCE)
+            for f_enc, f_loc in zip(face_encodings, face_locations):
+                results = face_recognition.compare_faces(known_faces, f_enc, TOLERANCE)
 
-                    if True in results:
-                        match = known_names[results.index(True)]
+                if True in results:
+                    match = known_names[results.index(True)]
 
-                        if DISPLAY_IMAGE:
-                            top_left = (f_loc[3], f_loc[0])
-                            bottom_right = (f_loc[1], f_loc[2])
-                            cv2.rectangle(cam_image, top_left, bottom_right, (0, 255, 0), FRAME_THICKNESS)
-                            cv2.putText(cam_image, match, (top_left[0], bottom_right[1] + 30),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), FONT_THICKNESS)
-
+                    if DISPLAY_IMAGE:
+                        top_left = (f_loc[3], f_loc[0])
+                        bottom_right = (f_loc[1], f_loc[2])
+                        cv2.rectangle(cam_image, top_left, bottom_right, (0, 255, 0), FRAME_THICKNESS)
+                        cv2.putText(cam_image, match, (top_left[0], bottom_right[1] + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), FONT_THICKNESS)
+                    else:
                         print(f"Match found: {match}")
 
-                        for e in emirates_id_list:
-                            if e.name == match:
-                                e.fine_amount += 3000  # Apply fine
+                match_phone = None
+                if trash_label == "trash" and previous_trash_label == "clean":
+                    for e in emirates_id_list:
+                        if e.name == match:
+                            e.fine_amount += 3000  # Apply fine
+                            match_phone = e.phone_number
+                            print(f"Applied fine to {e.name}")
+
+                distance = get_distance(trig_pin, echo_pin)
+
+                if abs(distance - previous_distance) > 3 and previous_distance != 0:
+                    print(f"Sending message to {match}...")
+                    send_msg(match, match_phone)
+                    GPIO.output(led_pin, GPIO.HIGH)
+                else:
+                    GPIO.output(led_pin, GPIO.LOW)
+
+                cv2.putText(cam_image, str(distance), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), FONT_THICKNESS)
+
+            previous_trash_label = trash_label
 
             if DISPLAY_IMAGE:
                 cv2.imshow("Camera Footage", cam_image)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-
-            previous_trash_label = trash_label
 
     except KeyboardInterrupt:
         exit_sequence(cap)
