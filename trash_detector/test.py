e34a6c87ef4448ee669c6c244b130941a467fda6
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-10-30 17:04:29 +0400

    updated model, testing data, and testing script

--- trash_detector/test.py
+++ trash_detector/test.py
@@ -1,33 +1,65 @@
 import os
-from tensorflow import keras
 import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image, ImageOps
+import cv2
 
-class_names = ["clean", "trash"]
-RESIZE_RES = (224, 224)
-model = keras.models.load_model("trash_detector/trash_detector_model.h5")
-folder_size = len(os.listdir("trash_detector/testing_data"))
-data = np.ndarray(shape=(folder_size, 224, 224, 3), dtype=np.float32)
+net = cv2.dnn.readNet("trash_detector/yolov3_training_last.weights", "trash_detector/yolov3_testing.cfg")
+classes = ["trash"]
+
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 # load testing data
 print("Loading testing images...")
-index = 0
 for filename in os.listdir("trash_detector/testing_data"):
     if filename != ".DS_Store":
-        image = image = Image.open(f'trash_detector/testing_data/{filename}')
-        resized = ImageOps.fit(image, RESIZE_RES, Image.ANTIALIAS)
-        image_array = np.asarray(resized)
-        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-        data[index] = normalized_image_array
-        index += 1
-print("Testing images loaded!")
+        img = cv2.imread(f"trash_detector/testing_data/{filename}")
+        img = cv2.resize(img, None, fx=0.4, fy=0.4)
+        height, width, channels = img.shape
 
-predictions = model.predict(data)
+        # Detecting objects
+        blob = cv2.dnn.blobFromImage(img, 0.004, (416, 416), (0, 0, 0), True, crop=False)
+        net.setInput(blob)
+        outs = net.forward(output_layers)
 
-for i in range(len(predictions)):
-    # display image with prediction
-    plt.title(f"Prediciction: {class_names[np.argmax(predictions[i])]}")
-    plt.xlabel(f"Confidence: {max(predictions[i])}")
-    plt.imshow(data[i])
-    plt.show()
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.3:
+                    # Object detected
+                    print(class_id)
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+                    # Rectangle coordinates
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        print(indexes)
+        font = cv2.FONT_HERSHEY_PLAIN
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                color = (255, 0, 0)
+                cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+                cv2.putText(img, label, (x, y + 30), font, 3, color, 2)
+
+        cv2.imshow("Image", img)
+        key = cv2.waitKey()
+
+        if key & 0xFF == ord('q'):
+            break
+
+cv2.destroyAllWindows()
